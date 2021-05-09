9467f1584011f57c38e0278663879a660b0cf9b4
karanbir-singh <karans1723@gmail.com>
2021-05-09 14:44:36 +0200

    Basic config pt.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
 import './App.less';
-import { Button, Steps } from 'antd';
 
 export default function App() {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <></>
   );
 }
